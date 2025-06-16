4442342be8d6b6aa4959d6e8bb7ee5741db2a810
Critical Sool <164774099+criticalsool@users.noreply.github.com>
2025-06-16 09:52:22 +0000

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# enshrouded-server
-Enshrouded Dedicated Server on Debian
+# Enshrouded server
+Enshrouded Dedicated Server install script on Debian
+
+# Install
+```bash
+git clone https://github.com/criticalsool/enshrouded-server.git
+cd enshrouded-server/
+bash enshrouded.bash
+```
+
+# Automation
+## Auto restart
+> As root
+```bash
+crontab -e
+10 7 * * * systemctl restart enshrouded.service
+```
+
+## Auto backup
+> As enshrouded
+```bash
+mkdir /home/enshrouded/backups
+crontab -e
+0 7 * * * tar -czvf /home/enshrouded/backups/enshrouded_backup_$(date +%Y-%m-%d).tar.gz -C /home/enshrouded/enshroudedserver/savegame/ .
+```
---
+++ enshrouded.bash
@@ -0,0 +1,53 @@
+#! /bin/bash
+
+# Install needed packages
+apt-get update
+apt-get install -y curl nano ufw wget software-properties-common lsb-release
+
+# Configure firewall
+ufw allow 15637/udp
+ufw default deny incoming
+ufw default deny forward
+ufw enable
+
+# Install winehq
+mkdir -pm755 /etc/apt/keyrings
+wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/$(lsb_release -is | tr '[:upper:]' '[:lower:]')/dists/$(lsb_release -cs)/winehq-$(lsb_release -cs).sources
+dpkg --add-architecture i386
+apt-get update
+apt-get install -y --install-recommends winehq-staging
+
+# Install steamcmd
+apt-add-repository non-free non-free-firmware
+apt-get update
+apt-get install steamcmd
+
+# Add enshrouded user
+useradd -m enshrouded
+
+# Install enshrouded service
+cat > /etc/systemd/system/enshrouded.service <<'EOF'
+# /etc/systemd/system/enshrouded.service
+[Unit]
+Description=Enshrouded Server
+Wants=network-online.target
+After=network-online.target
+[Service]
+User=enshrouded
+Group=enshrouded
+WorkingDirectory=/home/enshrouded/
+ExecStartPre=/usr/games/steamcmd +@sSteamCmdForcePlatformType windows +force_install_dir /home/enshrouded/enshroudedserver +login anonymous +app_update 2278520 +quit
+ExecStart=/usr/bin/wine /home/enshrouded/enshroudedserver/enshrouded_server.exe
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Enable enshrouded service at startup
+systemctl enable enshrouded
+
+systemctl status enshrouded
+
+# Log to enshrouded user
+#sudo -u enshrouded -s
